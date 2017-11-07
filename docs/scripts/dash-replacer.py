30841804925f1e48f7189ca81a10aa342abfee51
Jared Kobos <jaredkobos@gmail.com>
2017-11-07 10:51:12 -0500

    Finish script for dash replacement

--- docs/scripts/dash-replacer.py
+++ docs/scripts/dash-replacer.py
@@ -1,28 +1,42 @@
 import sys
 import regex
+import os
+import fnmatch
 
-filenames = []
-for fn in range(1,len(sys.argv)):
-    filenames.append(sys.argv[fn])
 
-target = regex.compile(r'-{4,}')
+def main():
+    print('Running replacement script...')
+    target = regex.compile(r'-{4,}')
+    source_dir = '.'
+    if len(sys.argv) > 1:
+        source_dir = sys.argv[1]
+    for root, dirnames, filenames in os.walk(source_dir):
+        for filename in fnmatch.filter(filenames,'*.md'):
+            old_line = ''
+            with open(os.path.join(root,filename)) as infile:
+                new_doc = ''
+                is_shortcode = []
+                for number, line in enumerate(infile, 1):
+                    if regex.findall('{{< \w+',line):
+                        is_shortcode.append(True)
+                    elif regex.findall('{{< \/',line):
+                        is_shortcode.pop()
+                    if line.startswith('|') or line.startswith('#') or any(is_shortcode):
+                        new_doc += old_line
+                    else:
+                        matches = regex.findall(target, line)
+                        if matches: 
+                            print("Found match in file {} at line {}:".format(filename, str(number)))
+                            print(matches)
+                            new_doc += '# ' + old_line
+                            line = ''
+                        else:
+                            new_doc += old_line
+                    old_line = line
+            new_doc += old_line
+            old_line = ''
+            with open(os.path.join(root,filename), 'w') as f:
+                f.write(new_doc)
 
-for filepath in filenames:
-    with open(filepath, 'r') as f:
-        old_line = ''
-        new_doc = ''
-        for number, line in enumerate(f,1):
-            if line.startswith('|'):
-                new_doc += old_line
-            else:
-                matches = regex.findall(target, line)
-                if matches: 
-                    print('Found match at line ' + str(number) + ':')
-                    new_doc += '# ' + old_line
-                    line = ''
-                else:
-                    new_doc += old_line
-            old_line = line
-    with open(filepath, 'w') as f:
-        f.write(new_doc)
-
+if __name__ == '__main__':
+    main()
