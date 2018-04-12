089cbedfff1e3d9a63b38c9a10bea423e0c5aeae
Sam Foo <sfoo@linode.com>
2018-04-12 14:42:49 -0400

    Added frontmatter to requirements; pass kwargs to functions

--- ci/test_frontmatter.py
+++ ci/test_frontmatter.py
@@ -4,7 +4,7 @@ import frontmatter
 from blueberry import require_yaml, only_allowed_yaml
 
 def test_required_yaml():
-    with open('ci/data/SHOUTING_BAD_FILE.md',r) as f:
+    with open('ci/data/SHOUTING_BAD_FILE.md','r') as f:
         yaml = frontmatter.loads(f.read())
         print(yaml)
 
