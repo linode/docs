bb39e4d327cf4a894b086bd7166984aa77831ac3
leslitagordita <contact@lesliesalazar.com>
2018-06-18 17:56:38 -0400

    Refactor for no errors found

--- ci/test_urls.py
+++ ci/test_urls.py
@@ -2,14 +2,19 @@ import requests
 import sys
 
 def test_urls(edge_cases):
+    hasErrors = False
     for edge in edge_cases:
         r =  requests.get(edge)
         if r.status_code == 404:
             print("Manual URL 404 test:",edge, "returned a 404")
-            sys.exit(1)
+            hasErrors = True
+    if hasErrors:
+        sys.exit(1)
+    else:
+        print("No 404s were found from manual test")
 
 def main():
-    edge_cases = ['http://localhost:1313/docs/contribute/thankyou']
+    edge_cases = ['http://localhost:1313/docs/contribute/thankyou', 'http://localhost:1313/docs/somefile']
     test_urls(edge_cases)
 
 main()
