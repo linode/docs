5d918f19f8ea57a65c9d2274671f9a8aa08e61d4
SamanthaFullerton <41454968+SamanthaFullerton@users.noreply.github.com>
2018-07-20 13:37:23 -0700

    Parse response code for start urls; handle shell script failure modes (#1917)

--- ci/docs404.py
+++ ci/docs404.py
@@ -37,11 +37,15 @@ class Docs404Spider(CrawlSpider):
              callback='parse_item', follow=True),
     )
 
+    def parse_start_url(self, response):
+        return self.parse_item(response)
+
     def parse_item(self, response):
         item = Docs404Item()
 
         if response.status == 404:
-            item['referer'] = response.request.headers.get('Referer')
+            ref = response.request.headers.get('Referer')
+            item['referer'] = ref if ref else 'orphaned link'
             item['status'] = response.status
             item['url'] = response.url
             return item
