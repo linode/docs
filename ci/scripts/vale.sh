1a0cf13c9fbb27fb7889fb2fc15174b933dba0de
nmelehan <nmelehan@gmail.com>
2018-11-28 21:25:08 +0000

    Fixing unzip line

--- ci/scripts/vale.sh
+++ ci/scripts/vale.sh
@@ -2,5 +2,5 @@
 
 wget https://github.com/errata-ai/vale/releases/download/v1.2.6/vale_1.2.6_Linux_64-bit.tar.gz
 mkdir vale
-tar -xvzf vale_1.2.6_macOS_64-bit.tar.gz -C vale
+tar -xvzf vale_1.2.6_Linux_64-bit.tar.gz -C vale
 ./vale/vale --glob='*.{md}' docs
