427a3df886ee332c4b3b030e428c420b17544659
Nathan <nmelehan@gmail.com>
2018-12-04 19:01:01 +0000

    Pin Vale to version 1.2.6 (#2155)
    * Pinning Vale to version 1.2.6
    
    * Fixing unzip line

--- ci/scripts/vale.sh
+++ ci/scripts/vale.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-go get github.com/ValeLint/vale
-vale --glob='*.{md}' docs
+wget https://github.com/errata-ai/vale/releases/download/v1.2.6/vale_1.2.6_Linux_64-bit.tar.gz
+mkdir vale
+tar -xvzf vale_1.2.6_Linux_64-bit.tar.gz -C vale
+./vale/vale --glob='*.{md}' docs
