629a94aeeeb399eb088b06c2c1e4cae4cb25b9ad
hzoppetti <hzoppetti@linode.com>
2020-08-27 18:25:31 -0400

    print out file list

--- ci/scripts/vale.sh
+++ ci/scripts/vale.sh
@@ -3,6 +3,8 @@
 wget https://github.com/errata-ai/vale/releases/download/v1.2.6/vale_1.2.6_Linux_64-bit.tar.gz
 mkdir vale
 tar -xvzf vale_1.2.6_Linux_64-bit.tar.gz -C vale
-git diff --name-only develop | grep 'index.md' | while read -r D ; do
-    ./vale/vale --glob=$D docs
+files=`git diff --name-only develop | grep 'index.md'`
+for file in $files; do
+  echo ${file}
 done
+./vale/vale --glob=${files} docs
