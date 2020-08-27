b365e5b839dc400c4f5ef1a3c996fdb7a1a51d93
hzoppetti <hzoppetti@linode.com>
2020-08-27 17:31:42 -0400

    changed script

--- ci/scripts/vale.sh
+++ ci/scripts/vale.sh
@@ -3,5 +3,5 @@
 wget https://github.com/errata-ai/vale/releases/download/v1.2.6/vale_1.2.6_Linux_64-bit.tar.gz
 mkdir vale
 tar -xvzf vale_1.2.6_Linux_64-bit.tar.gz -C vale
-d=git diff --name-only develop
-./vale/vale --glob=$d'.{md}' docs
+changed_files=`git diff --name-only develop`
+./vale/vale --glob=$changed_files'.{md}' docs
