c33ae54901f59964bcd1bc40a4eb4d195afaa4fa
hzoppetti <hzoppetti@linode.com>
2020-09-10 14:53:09 -0400

    trying a different travis variable

--- ci/scripts/vale.sh
+++ ci/scripts/vale.sh
@@ -3,8 +3,8 @@
 wget https://github.com/errata-ai/vale/releases/download/v1.2.6/vale_1.2.6_Linux_64-bit.tar.gz
 mkdir vale
 tar -xvzf vale_1.2.6_Linux_64-bit.tar.gz -C vale
-branch=$TRAVIS_PULL_REQUEST_BRANCH
-echo ${TRAVIS_PULL_REQUEST_BRANCH}
+branch=$TRAVIS_BRANCH
+echo ${branch}
 files=$(git diff --name-only ${branch} | grep 'index.md')
 for file in $files; do
   echo ${file}
