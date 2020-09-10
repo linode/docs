a79b5968c45cf45430d6cdaba8cab087ddab0140
hzoppetti <hzoppetti@linode.com>
2020-09-10 14:41:28 -0400

    updated command to posix style and outputing branch for testing

--- ci/scripts/vale.sh
+++ ci/scripts/vale.sh
@@ -3,7 +3,9 @@
 wget https://github.com/errata-ai/vale/releases/download/v1.2.6/vale_1.2.6_Linux_64-bit.tar.gz
 mkdir vale
 tar -xvzf vale_1.2.6_Linux_64-bit.tar.gz -C vale
-files=`git diff --name-only ${TRAVIS_PULL_REQUEST_BRANCH} | grep 'index.md'`
+branch=$TRAVIS_PULL_REQUEST_BRANCH
+echo ${TRAVIS_PULL_REQUEST_BRANCH}
+files=$(git diff --name-only ${branch} | grep 'index.md')
 for file in $files; do
   echo ${file}
 done
