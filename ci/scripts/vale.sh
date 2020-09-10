2b0dc59fc30718324c95e6dad7afa004de5ec80c
hzoppetti <hzoppetti@linode.com>
2020-09-10 16:37:16 -0400

    nothing commit to test new on develop

--- ci/scripts/vale.sh
+++ ci/scripts/vale.sh
@@ -6,7 +6,6 @@ tar -xvzf vale_1.2.6_Linux_64-bit.tar.gz -C vale
 branch=$TRAVIS_BRANCH
 echo "branch: "${branch}
 files=$(git diff --name-only origin/${branch} | grep 'index.md')
-
 for file in $files; do
   echo ${file}
 done
