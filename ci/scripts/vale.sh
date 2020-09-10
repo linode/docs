8b227a4c00fd7387893e3e26d670bc3b5354df36
hzoppetti <hzoppetti@linode.com>
2020-09-10 16:30:39 -0400

    update to force travis tests

--- ci/scripts/vale.sh
+++ ci/scripts/vale.sh
@@ -5,7 +5,6 @@ mkdir vale
 tar -xvzf vale_1.2.6_Linux_64-bit.tar.gz -C vale
 branch=$TRAVIS_BRANCH
 echo "branch: "${branch}
-
 files=$(git diff --name-only origin/${branch} | grep 'index.md')
 
 for file in $files; do
