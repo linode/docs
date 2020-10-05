69fe47479e11baace7a3864f1cebed7f25696956
nmelehan <nmelehan@gmail.com>
2020-10-05 12:53:00 -0400

    Update theme

--- _vendor/github.com/bep/linodedocs/assets/js/main/sections/home/home.js
+++ _vendor/github.com/bep/linodedocs/assets/js/main/sections/home/home.js
@@ -24,7 +24,7 @@ var lnHome = {};
 		// The section names we paginate on the home page.
 		// This maps to the name attribute in the search configuration.
 		// No filters are currently applied, and the order will be the order from Algolia.
-		const sectionNames = [ 'docs', 'blog', 'resources', 'marketplace', 'qa' ];
+		const sectionNames = [ 'guides', 'blog', 'resources', 'marketplace', 'qa' ];
 
 		// Create a new pager for the given el and items.
 		// pageSize is the number of items per page.
