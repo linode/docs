f1a2f666d4709dae579b7cc0e1c1c687da29c0b0
nmelehan <nmelehan@gmail.com>
2020-10-21 02:58:53 -0400

    Add DEPLOY_SUFFIX to generate_permanent_redirects.sh, update script comments

--- scripts/generate_permanent_redirects.sh
+++ scripts/generate_permanent_redirects.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Usage:
+#    DEPLOY_SUFFIX=-suffix-for-obj-bucket-name ./generate_permanent_redirects.sh
+#
+# This script iterates through the public/ folder and searches for any pages
+# created from Hugo's `aliases` frontmatter. It then applies the
+# `x-amz-website-redirect-location` S3 header to that page in object storage.
+# This header will serve a 301 redirect for requests on the page. Without this
+# header, the page would serve a browser-based redirect.
+#
+# The script assumes that you have already synced the public/ folder to the
+# object storage bucket.
+#
+# The name of the object storage bucket will be of the form
+# `linodedocs$DEPLOY_SUFFIX`, where `DEPLOY_SUFFIX` is passed as an environment
+# variable. For example, setting `DEPLOY_SUFFIX=-latestrelease` will deploy to
+# our production Object Storage bucket, which is `linodedocs-latestrelease`, in
+# the us-east cluster.
+
 egrep -R '<!DOCTYPE html><html><head><title>(https:\/\/.*\/)<\/title><link rel="canonical" href="(https:\/\/.*\/)"\/><meta name="robots" content="noindex"><meta charset="utf-8" \/><meta http-equiv="refresh" content="0; url=(https:\/\/.*\/)" \/><\/head><\/html>' ../public | while read -r line ; do
     # Gets just the relative path that the redirect should point to
     # Example: /docs/databases/mariadb/how-to-install-mariadb-on-centos-7/
@@ -9,5 +27,5 @@ egrep -R '<!DOCTYPE html><html><head><title>(https:\/\/.*\/)<\/title><link rel="
     redirect_from_path="$(echo $line | cut -d ':' -f 1 | cut -c 10-)"
 
     echo "Redirecting /docs$redirect_from_path to $redirect_target_path"
-    s3cmd put --no-mime-magic --acl-public --no-preserve --add-header=x-amz-website-redirect-location:$redirect_target_path ../public$redirect_from_path s3://linodedocs-latestrelease/docs$redirect_from_path
+    s3cmd put --no-mime-magic --acl-public --no-preserve --add-header=x-amz-website-redirect-location:$redirect_target_path ../public$redirect_from_path s3://linodedocs$DEPLOY_SUFFIX/docs$redirect_from_path
 done
