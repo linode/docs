31feb2a7c241077b12d3aa136d810c47c79a8852
leslitagordita <contact@lesliesalazar.com>
2020-10-28 12:38:46 -0400

    Theme changes for Oct 28 2020
    Turbolinks fix for css styling and weglot lang switcher
    Updates to header links
    Fix product docs guide sections

--- _vendor/github.com/bep/linodedocs/assets/js/main/main.js
+++ _vendor/github.com/bep/linodedocs/assets/js/main/main.js
@@ -13,10 +13,25 @@ var LN_DEBUG = 0;
 	document.addEventListener('turbolinks:click', function(event) {
 		const anchorElement = event.target.closest('a');
 
-		var noTurbolink =
-			anchorElement.hash &&
-			anchorElement.origin === window.location.origin &&
-			anchorElement.pathname === window.location.pathname;
+		// Turbolinks intercepts all clicks on <a href> links to the same domain.
+		// The documentation is hosted at linode.com/docs, but there are links
+		// to other sections of that same domain, so prevent Turbolinks from handling those.
+		// pathname is a USVString containing an initial '/' followed by the path of the URL
+		//(or the empty string if there is no path).
+		var noTurbolink = !anchorElement.pathname.startsWith('/docs/');
+
+		if (noTurbolink) {
+			// Prepare a clean state for when the user comes back.
+			Turbolinks.clearCache();
+		}
+
+		if (!noTurbolink) {
+			// Prevent Turbolinks from handling #hash locations on the same page.
+			noTurbolink =
+				anchorElement.hash &&
+				anchorElement.origin === window.location.origin &&
+				anchorElement.pathname === window.location.pathname;
+		}
 
 		if (noTurbolink) {
 			event.preventDefault();
