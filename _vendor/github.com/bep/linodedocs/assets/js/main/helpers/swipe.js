a1f29380e584ea7aae765438db069eab622761b1
Nathan Melehan <nmelehan@gmail.com>
2020-12-21 15:40:36 -0500

    Revert "[Hotfix] v1.12.2"

--- _vendor/github.com/bep/linodedocs/assets/js/main/helpers/swipe.js
+++ _vendor/github.com/bep/linodedocs/assets/js/main/helpers/swipe.js
@@ -1,65 +1,69 @@
-'use strict';
+var lnSwipe = {};
 
-export function newSwiper(el, callback) {
-	// Number of pixels between touch start/end for us to consider it a swipe.
-	const moveThreshold = 50;
+(function(ctx) {
+	'use strict';
 
-	var touches = {
-		touchstart: { x: -1, y: -1 },
-		touchmove: { x: -1, y: -1 }
-	};
+	ctx.New = function(el, callback) {
+		// Number of pixels between touch start/end for us to consider it a swipe.
+		const moveThreshold = 50;
 
-	// direction returns right or left if above moveThreshold,
-	// empty if not.
-	// Note that we have not implemented movements along the
-	// y axis.
-	touches.direction = function() {
-		if (this.touchmove.x == -1) {
-			// A regular click.
-			return '';
-		}
+		var touches = {
+			touchstart: { x: -1, y: -1 },
+			touchmove: { x: -1, y: -1 }
+		};
 
-		let distance = this.touchmove.x - this.touchstart.x;
-		if (Math.abs(distance) < moveThreshold) {
-			return '';
-		}
-		return distance > 0 ? 'right' : 'left';
-	};
-
-	touches.reset = function() {
-		(this.touchstart.x = -1), (this.touchstart.y = -1);
-		(this.touchmove.x = -1), (this.touchmove.y = -1);
-	};
-
-	touches.update = function(event, touch) {
-		this[event.type].x = touch.pageX;
-		this[event.type].y = touch.pageY;
-	};
-
-	var handleTouch = function(event) {
-		if (typeof event !== 'undefined' && typeof event.touches !== 'undefined') {
-			var touch = event.touches[0];
-			switch (event.type) {
-				case 'touchstart':
-					touches.reset();
-					touches.update(event, touch);
-					break;
-				case 'touchmove':
-					touches.update(event, touch);
-					break;
-				case 'touchend':
-					let direction = touches.direction();
-					if (direction) {
-						callback(direction);
-					}
-					break;
-				default:
-					break;
+		// direction returns right or left if above moveThreshold,
+		// empty if not.
+		// Note that we have not implemented movements along the
+		// y axis.
+		touches.direction = function() {
+			if (this.touchmove.x == -1) {
+				// A regular click.
+				return '';
 			}
-		}
-	};
 
-	el.addEventListener('touchstart', handleTouch, true);
-	el.addEventListener('touchmove', handleTouch, true);
-	el.addEventListener('touchend', handleTouch, false);
-}
+			let distance = this.touchmove.x - this.touchstart.x;
+			if (Math.abs(distance) < moveThreshold) {
+				return '';
+			}
+			return distance > 0 ? 'right' : 'left';
+		};
+
+		touches.reset = function() {
+			(this.touchstart.x = -1), (this.touchstart.y = -1);
+			(this.touchmove.x = -1), (this.touchmove.y = -1);
+		};
+
+		touches.update = function(event, touch) {
+			this[event.type].x = touch.pageX;
+			this[event.type].y = touch.pageY;
+		};
+
+		var handleTouch = function(event) {
+			if (typeof event !== 'undefined' && typeof event.touches !== 'undefined') {
+				var touch = event.touches[0];
+				switch (event.type) {
+					case 'touchstart':
+						touches.reset();
+						touches.update(event, touch);
+						break;
+					case 'touchmove':
+						touches.update(event, touch);
+						break;
+					case 'touchend':
+						let direction = touches.direction();
+						if (direction) {
+							callback(direction);
+						}
+						break;
+					default:
+						break;
+				}
+			}
+		};
+
+		el.addEventListener('touchstart', handleTouch, true);
+		el.addEventListener('touchmove', handleTouch, true);
+		el.addEventListener('touchend', handleTouch, false);
+	};
+})(lnSwipe);
