188f2edcfc0d2d9a7014403e00abdfdf29b99f84
nmelehan <nmelehan@gmail.com>
2021-01-26 11:25:36 -0500

    Revert "Merge pull request #3935 from nmelehan/hotfix-v1.15.3"
    This reverts commit 49c9967cf7db862eda67d97cc4b1c311ed0162e9, reversing
    changes made to afe4624416ad99572a8ac56de0f861ad51ebcaa0.

--- _vendor/github.com/bep/linodedocs/assets/js/main/components/dropdowns.js
+++ _vendor/github.com/bep/linodedocs/assets/js/main/components/dropdowns.js
@@ -1,58 +1,63 @@
-'use strict';
+var lnDropdowns = {};
 
-var debug = 0 ? console.log.bind(console, '[dropdowns]') : function() {};
+(function(ctx) {
+	'use strict';
 
-export function newDropdownsController(dropdowns) {
-	return {
-		dropdowns: dropdowns,
-		init: function() {
-			if (document.body.classList.contains('is-topbar-pinned')) {
-				this.dropdowns.forEach((e) => {
-					if (e.hideWhenPinned) {
-						e.hidden = true;
-					}
-				});
-			}
-		},
-		toggleOpen: function(idx) {
-			let wasOpen = this.dropdowns[idx].open;
-			this.dropdowns[idx].open = !wasOpen;
+	var debug =
+		(typeof LN_DEBUG !== 'undefined' && LN_DEBUG) || 0 ? console.log.bind(console, '[dropdowns]') : function() {};
 
-			if (wasOpen) {
-				return;
-			}
-
-			// Only 1 open at a time.
-			for (let i in dropdowns) {
-				if (i != idx) {
-					this.dropdowns[i].open = false;
-				}
-			}
-		},
-		closeAll: function() {
-			this.dropdowns.forEach((e) => {
-				e.open = false;
-			});
-		},
-		isOpen: function(idx) {
-			return this.dropdowns[idx].open;
-		},
-		isHidden: function(idx) {
-			return this.dropdowns[idx].hidden;
-		},
-		receiveToggle: function(detail) {
-			debug('receiveToggle', detail);
-			switch (detail.what) {
-				case 'topbar-pinned':
+	ctx.New = function(dropdowns) {
+		return {
+			dropdowns: dropdowns,
+			init: function() {
+				if (document.body.classList.contains('is-topbar-pinned')) {
 					this.dropdowns.forEach((e) => {
 						if (e.hideWhenPinned) {
-							e.hidden = detail.open;
+							e.hidden = true;
 						}
 					});
-					break;
-				default:
-				// Ignore
+				}
+			},
+			toggleOpen: function(idx) {
+				let wasOpen = this.dropdowns[idx].open;
+				this.dropdowns[idx].open = !wasOpen;
+
+				if (wasOpen) {
+					return;
+				}
+
+				// Only 1 open at a time.
+				for (let i in dropdowns) {
+					if (i != idx) {
+						this.dropdowns[i].open = false;
+					}
+				}
+			},
+			closeAll: function() {
+				this.dropdowns.forEach((e) => {
+					e.open = false;
+				});
+			},
+			isOpen: function(idx) {
+				return this.dropdowns[idx].open;
+			},
+			isHidden: function(idx) {
+				return this.dropdowns[idx].hidden;
+			},
+			receiveToggle: function(detail) {
+				debug('receiveToggle', detail);
+				switch (detail.what) {
+					case 'topbar-pinned':
+						this.dropdowns.forEach((e) => {
+							if (e.hideWhenPinned) {
+								e.hidden = detail.open;
+							}
+						});
+						break;
+					default:
+					// Ignore
+				}
 			}
-		}
+		};
 	};
-}
+})(lnDropdowns);
