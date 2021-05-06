40af64cb6cdf00ddca24222d516b508927da2f49
Sachin <sachin301190@gmail.com>
2021-05-06 21:20:43 +0530

    Deleted var.sh

--- var.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-echo 'The current logged in User is:' $USER
-echo 'Home directory of the current user is:' $HOME
-echo 'Pathname of the current users shell is:' $SHELL
-echo 'The Present Working Directory is:' $PWD
-echo 'Users unique identifier is:' $UID
