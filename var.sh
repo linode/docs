330dd3dd585924d429319747161a99c18ef1573a
Sachin <sachin301190@gmail.com>
2021-05-06 20:56:41 +0530

    Renaming AWK folder

---
+++ var.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+echo 'The current logged in User is:' $USER
+echo 'Home directory of the current user is:' $HOME
+echo 'Pathname of the current users shell is:' $SHELL
+echo 'The Present Working Directory is:' $PWD
+echo 'Users unique identifier is:' $UID
