cabb24016c254bf8d6b356350e9b15d712cd89f3
Nathan Melehan <nmelehan@gmail.com>
2023-01-24 14:43:52 -0500

    Diagnose hugo server problem, take 3

--- ci/docs404.py
+++ ci/docs404.py
@@ -57,9 +57,9 @@ if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex(('127.0.0.1',1313))
     if result == 0:
-        print "Port is open"
+        print("Port is open")
     else:
-        print "Port is not open"
+        print("Port is not open")
     sock.close()
 
 
