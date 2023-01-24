44960a2e29e516fc5432c9fa1af68e7434de39f9
Nathan Melehan <nmelehan@gmail.com>
2023-01-24 14:48:55 -0500

    Diagnose hugo server problem, take 4

--- ci/docs404.py
+++ ci/docs404.py
@@ -28,8 +28,8 @@ class Docs404Spider(CrawlSpider):
 #    Delay if server is returning lots of 500s
 #    DOWNLOAD_DELAY=0.1
     name = 'docs404'
-    allowed_domains = ['localhost' ]
-    start_urls = ['http://localhost:1313/docs']
+    allowed_domains = ['127.0.0.1' ]
+    start_urls = ['http://127.0.0.1:1313/docs']
     handle_httpstatus_list = [404]
 
     rules = (
