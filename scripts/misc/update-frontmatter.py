fe1d75e08753c6ad6d5f28dab05b76b17c31bfc8
Matthew Wildman <matt@wildman.online>
2024-04-18 11:27:38 -0400

    Updates to the update-frontmatter.py script

--- scripts/misc/update-frontmatter.py
+++ scripts/misc/update-frontmatter.py
@@ -198,12 +198,141 @@ def remove_duplicate_parameters():
                                 fp.write(line)
 
 # ------------------
+# Reorder front matter
+# ------------------
+def sort_parameters():
+
+    # Iterate through each file in each docs directory
+    for dir in DOCS_DIR:
+        for root, dirs, files in os.walk(dir):
+            for file in files:
+
+                # The relative file path of the file
+                file_path = os.path.join(root, file)
+                path_segments = file_path.split("/")
+
+                # If the file is markdown...
+                if file.endswith('.md'):
+
+                    with open(file_path, "r") as fp:
+                        lines = fp.readlines()
+
+                    slug = ""
+                    title = ""
+                    title_meta = ""
+                    description = ""
+                    description_meta = ""
+                    authors = ""
+                    contributors = ""
+                    modified = ""
+                    published = ""
+
+                    frontmatter = False
+                    yaml_token = "---"
+                    yaml_token_counter = 0
+                    update = False
+                    duplicate_date = False
+
+                    # Iterates through each line of the file and locates
+                    # the title and description parameters.
+                    for i, line in enumerate(lines):
+                        if line.startswith(yaml_token) and yaml_token_counter == 0:
+                            yaml_token_counter += 1
+                            frontmatter = True
+                        elif line.startswith(yaml_token) and yaml_token_counter == 1:
+                            yaml_token_counter += 1
+                            frontmatter = False
+
+                        if frontmatter == False:
+                            continue
+
+                        if line.startswith("slug:"):
+                            slug = line
+                        elif line.startswith("title:"):
+                            title = line
+                        elif line.startswith("title_meta:"):
+                            title_meta = line
+                        elif line.startswith("description:"):
+                            description = line
+                        elif line.startswith("description_meta:"):
+                            description_meta = line
+                        elif line.startswith("authors:"):
+                            authors = line
+                        elif line.startswith("contributors:"):
+                            contributors = line
+                        elif line.startswith("published:"):
+                            published = line
+                        elif line.startswith("modified:"):
+                            modified = line
+
+                    # Reset the yaml token counter
+                    yaml_token_counter = 0
+
+                    # Write to the file
+                    with open(file_path, "w") as fp:
+
+
+                        for line in lines:
+
+                            if line.startswith(yaml_token) and yaml_token_counter == 0:
+                                yaml_token_counter += 1
+                                frontmatter = True
+                            elif line.startswith(yaml_token) and yaml_token_counter == 1:
+                                yaml_token_counter += 1
+                                frontmatter = False
+
+                            if frontmatter:
+                                if line.startswith("---"):
+                                    fp.write("---\n")
+                                    if not slug == "":
+                                        fp.write(slug)
+                                    if not title == "":
+                                        fp.write(title)
+                                    if not title_meta == "":
+                                        fp.write(title_meta)
+                                    if not description == "":
+                                        fp.write(description)
+                                    if not description_meta == "":
+                                        fp.write(description_meta)
+                                    if not authors == "":
+                                        fp.write(authors)
+                                    if not contributors == "":
+                                        fp.write(contributors)
+                                    if not published == "":
+                                        fp.write(published)
+                                    if not modified == "":
+                                        fp.write(modified)
+                                elif line.startswith("slug"):
+                                    continue
+                                elif line.startswith("title"):
+                                    continue
+                                elif line.startswith("title_meta"):
+                                    continue
+                                elif line.startswith("description"):
+                                    continue
+                                elif line.startswith("description_meta"):
+                                    continue
+                                elif line.startswith("authors"):
+                                    continue
+                                elif line.startswith("contributors"):
+                                    continue
+                                elif line.startswith("published"):
+                                    continue
+                                elif line.startswith("modified"):
+                                    continue
+                                else:
+                                    fp.write(line)
+                            else:
+                                fp.write(line)
+
+# ------------------
 # Main function
 # ------------------
 def main():
 
-    update_titles()
-    remove_duplicate_parameters()
+    #update_titles()
+    #remove_duplicate_parameters()
+    sort_parameters()
 
 if __name__ == "__main__":
     main()
